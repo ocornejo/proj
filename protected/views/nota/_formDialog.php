bef654b50c6315c12d4c08dd10c8730d840bf3be
Oscar Cornejo <o.cornejo.o@gmail.com>
2013-02-17 21:42:30 +0000

    commit domingo

--- protected/views/nota/_formDialog.php
+++ protected/views/nota/_formDialog.php
@@ -3,7 +3,6 @@
 <?php 
 $baseUrl = Yii::app()->theme->baseUrl; 
 $cs = Yii::app()->getClientScript();
-//document.getElementById('nombre[0]').innerHTML='hola';
 $cs->registerCssFile($baseUrl.'/css/jquery.css');
 $form=$this->beginWidget('CActiveForm', array(
     'id'=>'nota-form',
@@ -16,12 +15,26 @@ $form=$this->beginWidget('CActiveForm', array(
 <script type="text/javascript">
         window.arreglo= new Array();
         window.global=1;
-        function inicia(var1){
-            
-            for(var i=0;i<var1.length;i++){
-                window.arreglo[i]=0;
-                console.log("llenando: "+window.arreglo[i]);
+        window.notasFinales=new Array();
+        
+        Object.size = function(obj) {
+            var size = 0, key;
+            for (key in obj) {
+                if (obj.hasOwnProperty(key)) size++;
             }
+            return size;
+        };
+
+        function inicia(var1){
+            temp = new Object();
+            for(var i=0;i<var1.length;i++){
+                temp[var1[i]['evaluacion_id_evaluacion']]=parseInt(var1[i]['evaluacion_id_evaluacion']);
+                window.arreglo[i]=0;   
+                
+            }
+            console.log("tempCount: "+Object.size(temp));
+            //crear un arreglo del largo de la wea de arriba y guardar alli las notas y calcularas en la funcion de abajo
+            
         }
         function updateTag(var1,var2){
             final=0;
@@ -32,21 +45,21 @@ $form=$this->beginWidget('CActiveForm', array(
             for(var j=0;j<var2.length;j++){
                 if(var2[j]['evaluacion_id_evaluacion'] == var1){
                     count=count+1;
-                    total = total + parseInt(window.arreglo[j]);
-                    console.log("total :"+total);
+                    total = total + parseFloat(window.arreglo[j]);
+                    console.log("total :"+total+" count: "+count+" parse: "+parseFloat(window.arreglo[j]));
                 }
+                
            }
-           
-           final=total/count;
+           //for(var i=0;i<temp.)
+           final=Number(((total/count)*100).toFixed(0));
            console.log("final: "+final);
-           variable1= "nombre["+var1+"]";
-           <?php //print("console.log(variable1);");?>
-           document.getElementById(variable1).innerHTML='hola';
+           var1= var1-1;
+           variable1= "notaPond["+var1+"]";
+           
+           document.getElementById(variable1).innerHTML=final+"%";
 
 
-        }
-        
-        
+        } 
         </script>    
  
 
@@ -61,28 +74,28 @@ $form=$this->beginWidget('CActiveForm', array(
     </table>
     <table valign="top" style="font-size:large;">
  <?php for ($i = 0; $i < count($sql); $i++) {
+       
      if((int)$sql[$i]['ponderacion']!=0){
         echo '<tr><td><br></td></tr> 
               <tr>
-                    <td><h1>'.CHtml::label($sql[$i]['nombre'], 'nombre', array('id'=>'nombre['.$i.']')).'</h1></td>
-                    <td><h1>'.CHtml::label($sql[$i]['ponderacion'].'%', 'ponderacion').'</h1></td>
+                    <td><h1>'.CHtml::label($sql[$i]['nombre'].'  '.$sql[$i]['ponderacion'].'%', 'nombre', array('id'=>'nombre['.$i.']')).'</h1></td>
+                    <td><h1>'.CHtml::label('0%', 'ponderacion',array('id'=>'notaPond['.$i.']')).'</h1></td>
                 </tr>';
         for ($j = $i; $j < count($sql2); $j++) {
             if($sql2[$j]['evaluacion_id_evaluacion']==$sql[$i]['id_evaluacion']){
                 echo '<tr>
                 <td>'. CHtml::label($sql2[$j]['nombre'], 'nombreItem') .'</td>
-                <td>'. $form->radioButtonList($model,'NOTA', array('0'=>'1','1'=>'2','2'=>'3','3'=>'4','4'=>'5'), array('name'=>$sql2[$j]['item_id_item'],'id'=>'item['.$j.']','separator'=>' | ','onchange'=>'{
+                <td>'. $form->radioButtonList($model,'NOTA', array('0'=>'1','0.25'=>'2','0.5'=>'3','0.75'=>'4','1'=>'5'), array('name'=>$sql2[$j]['item_id_item'],'id'=>'item['.$j.']','separator'=>' | ','onchange'=>'{
                                                                                                                                                                                                                if(window.global==1){
                                                                                                                                                                                                                     inicia('.json_encode($sql2).');
                                                                                                                                                                                                                     window.global=0;
                                                                                                                                                                                                                } 
-                                                                                                                                                                                                                
+                                                                                                                                                                                                               window.arreglo['.$j.']=$(this).val();
                                                                                                                                                                                                                updateTag('.$i.','.json_encode($sql2).');
                                                                                                                                                                                                                     
-                                                                                                                                                                                                                window.arreglo['.$j.']=$(this).val();
+                                                                                                                                                                                                                
                                                                                                                                                                                                                 }')) .'</td>
-                
-                </tr>';
+                    </tr>';
                 
             }
         }
